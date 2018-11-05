ec01d32785d3427d1da64fe48c540d2909303b09
wadeLiang <694198985@qq.com>
2018-11-05 14:43:09 +0800

    Update PayExcelAction.java

--- src/main/java/com/jiang/work_sys/action/PayExcelAction.java
+++ src/main/java/com/jiang/work_sys/action/PayExcelAction.java
@@ -22,7 +22,7 @@ public class PayExcelAction {
 	@RequestMapping("b")
 	public void uploadPayExcelChange(HttpServletRequest req, HttpServletResponse rep, @RequestParam("file") File file)
 			throws IOException {
-		System.out.println("1212");
+		System.out.println("12dsaddfdsfds12");
 		rep.getWriter().write("d12a");
 		rep.getWriter().flush();
 		rep.getWriter().close();
